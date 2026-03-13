cf1e6b89f5a269f7a567573197f0731bcb239bef
Jason Ciayadi <christopher.ciayadi2511@gmail.com>
2026-03-13 07:20:42 +0700

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="py-8 text-center bg-slate-950 border-t border-slate-800 text-slate-400 text-sm">
       <p>
-        © {new Date().getFullYear()} Jason Christopher Ciayadi. All rights reserved.
+        © {new Date().getFullYear()} Jason Christopher. All rights reserved.
       </p>
     </footer>
   )
